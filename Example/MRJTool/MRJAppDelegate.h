d3de5214cb0ce9956dc3ddd43f20e280d4e2754d
余洪江 <>
2018-04-02 19:49:45 +0800

    Initial commit

---
+++ Example/MRJTool/MRJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MRJAppDelegate.h
+//  MRJTool
+//
+//  Created by mrjlovetian@gmail.com on 04/02/2018.
+//  Copyright (c) 2018 mrjlovetian@gmail.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface MRJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

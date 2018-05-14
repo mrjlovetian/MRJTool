7584641fbf0d28552c414af825faa101dfc87bb8
余洪江 <yuhongjiang642@tops001.com>
2018-05-14 10:42:25 +0800

    0.1.2.6

--- MRJTool/Classes/Marco/ScreenMacro.h
+++ MRJTool/Classes/Marco/ScreenMacro.h
@@ -10,16 +10,16 @@
 #define ScreenMacro_h
 
 #define ScreenSzie [[UIScreen mainScreen] bounds].size
-#define SreenWidth (ScreenSzie.width)
-#define SreenBottomHeight (iPhoneX ? 34 : 0)
-#define SreenHeight (iPhoneX?(ScreenSzie.height):ScreenSzie.height)
+#define ScreenWidth (ScreenSzie.width)
+#define ScreenBottomHeight (iPhoneX ? 34 : 0)
+#define ScreenHeight (iPhoneX?(ScreenSzie.height):ScreenSzie.height)
 #define NavBarHeight (iPhoneX ? 88 : 64)//bar的高度
 #define StatusBarHeight [[UIApplication sharedApplication] statusBarFrame].size.height
 #define VScaleRate (iPhone6Plus ? 1 : 2.0/3.0)
 #define VScaleRateWidth ScreenSzie / 320.0 //算宽度比率
-#define VScaleRateNewWidth SreenWidth / 375.0 //以6s为基准计算比例
-#define VScaleRateHeight SreenHeight / 736.0 //算高度比率
-#define VScaleRateNewHeight SreenHeight / 667 //算高度比率
+#define VScaleRateNewWidth ScreenWidth / 375.0 //以6s为基准计算比例
+#define VScaleRateHeight ScreenHeight / 736.0 //算高度比率
+#define VScaleRateNewHeight ScreenHeight / 667 //算高度比率
 #define VGetX(x) x*VScaleRateHeight;
 #define Edge 15 //边距15
 #define RowHeight 50 //行高，1行选择的高度
